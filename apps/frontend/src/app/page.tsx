522fad394051df7d54ae412d7b052c566bacfa0c
Evan Keane <web3evankeane@gmail.com>
2025-10-15 20:13:29 +0700

    Done initialization

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,3 @@
-'use client';
-
-import { ArrowRight, Check, Github, Terminal } from 'lucide-react';
-import Link from 'next/link';
-import { useState } from 'react';
-
 export default function Home() {
   return (
     <div className="container mx-auto flex min-h-screen items-center justify-center">
